9aaa295c260285351815535755cace8f45716dcf
elvesbd <elvesbd@yahoo.com>
2023-07-27 18:17:51 -0300

    feat: create implicit operator

--- UtmBuilder.Core/Extensions/ListExtension.cs
+++ UtmBuilder.Core/Extensions/ListExtension.cs
@@ -2,8 +2,9 @@ namespace UtmBuilder.Core.Extensions;
 
 public static class ListExtension
 {
-    public static void AddIfNotNull(this List<string> list, List<string> segments)
+    public static void AddIfNotNull(this List<string> list, string key, string? value)
     {
-
+        if (!string.IsNullOrEmpty(value))
+            list.Add($"{key}={value}");
     }
 }
--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -21,10 +21,18 @@ public class Utm
     /// </summary>
     public Campaign Campaign { get; }
 
+    public static implicit operator string(Utm utm)
+        => utm.ToString();
+
     public override string ToString()
     {
         var segments = new List<string>();
-        segments.AddIfNotNull(segments);
+        segments.AddIfNotNull("utm_source", Campaign.Source);
+        segments.AddIfNotNull("utm_medium", Campaign.Medium);
+        segments.AddIfNotNull("utm_campaign", Campaign.Name);
+        segments.AddIfNotNull("utm_id", Campaign.Id);
+        segments.AddIfNotNull("utm_term", Campaign.Term!);
+        segments.AddIfNotNull("utm_content", Campaign.Content!);
 
         return $"{Url.Address}?{string.Join("&", segments)}";
     }
