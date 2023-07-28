b7b34528e832557e4855d8a1985651d805429db7
elvesbd <elvesbd@yahoo.com>
2023-07-28 17:42:07 -0300

    test: add unit tests for Url

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -1,11 +1,32 @@
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
 namespace UtmBuilder.Core.Tests;
 
 [TestClass]
 public class UrlTests
 {
+    private const string InvalidUrl = "invalid_url";
+    private const string ValidUrl = "https://balta.io";
+
     [TestMethod]
+    [ExpectedException(typeof(InvalidUrlException))]
+    public void ShouldReturnExceptionWhenUrlIsNull()
+    {
+        var url = new Url(null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidUrlException))]
     public void ShouldReturnExceptionWhenUrlIsInvalid()
     {
+        var url = new Url(InvalidUrl);
+    }
 
+    [TestMethod]
+    public void ShouldNotReturnExceptionWhenUrlIsValid()
+    {
+        var url = new Url(ValidUrl);
+        Assert.IsTrue(true);
     }
 }
