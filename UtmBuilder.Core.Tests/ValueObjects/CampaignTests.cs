bf99e60245d023e6d32cb647ab392a6281813eed
elvesbd <elvesbd@yahoo.com>
2023-07-28 17:57:03 -0300

    test: add unit tests for Campaign

---
+++ UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
@@ -0,0 +1,43 @@
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests;
+
+[TestClass]
+public class CampaignTests
+{
+    private const string InvalidUrl = "invalid_url";
+    private const string ValidUrl = "https://balta.io";
+
+    [TestMethod]
+    [DataRow("", "", "", true)]
+    [DataRow("source", "", "", true)]
+    [DataRow("source", "medium", "", true)]
+    [DataRow("source", "medium", "name", false)]
+    public void TestCampaign(
+        string source,
+        string medium,
+        string name,
+        bool expectException
+    )
+    {
+        if (expectException)
+        {
+            try
+            {
+                new Campaign(source, medium, name);
+                Assert.Fail();
+            }
+            catch (InvalidCampaignException e)
+                when (e.Message == "Source is invalid")
+            {
+                Assert.IsTrue(true);
+            }
+        }
+        else
+        {
+            new Campaign(source, medium, name);
+            Assert.IsTrue(true);
+        }
+    }
+}
